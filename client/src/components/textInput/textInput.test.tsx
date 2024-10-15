c398cb61ad79a910df5c97f91842b645b71598c3
Brayan Durazo <brayan9612@gmail.com>
2024-10-15 12:44:33 -0700

    add test for text input

---
+++ client/src/components/textInput/textInput.test.tsx
@@ -0,0 +1,75 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import TextInput from './textInput';
+
+const setTextContentMock = jest.fn();
+
+describe('<TextInput />', () => {
+  const textValue = 'title';
+  const textContent = 'Welcome';
+  const isRequired = true;
+  test('renders TextInput', () => {
+    render(
+      <TextInput
+        textValue={textValue}
+        textContent={textContent}
+        isRequired={isRequired}
+        setTextContent={setTextContentMock}
+      />
+    );
+    expect(screen.queryByTestId('test-input-' + textValue)).toBeTruthy();
+  });
+
+  test('calls setTextContent when input received', async () => {
+    render(
+      <TextInput
+        textValue={textValue}
+        textContent={textContent}
+        isRequired={isRequired}
+        setTextContent={setTextContentMock}
+      />
+    );
+    expect(screen.queryByTestId('test-input-' + textValue)).toBeTruthy();
+    await userEvent.type(
+      screen.getByTestId('test-input-' + textValue),
+      'some text'
+    );
+    expect(setTextContentMock).toHaveBeenCalled();
+  });
+
+  test('shows text content', () => {
+    render(
+      <TextInput
+        textValue={textValue}
+        textContent={textContent}
+        isRequired={isRequired}
+        setTextContent={setTextContentMock}
+      />
+    );
+    expect(screen.queryByDisplayValue(textContent)).toBeTruthy();
+  });
+  test('shows symbol if required', () => {
+    render(
+      <TextInput
+        textValue={textValue}
+        textContent={''}
+        isRequired={isRequired}
+        setTextContent={setTextContentMock}
+      />
+    );
+    expect(screen.queryByPlaceholderText(textValue + '*')).toBeTruthy();
+  });
+
+  test('does not shows symbol if not required', () => {
+    render(
+      <TextInput
+        textValue={textValue}
+        textContent={''}
+        isRequired={false}
+        setTextContent={setTextContentMock}
+      />
+    );
+    expect(screen.queryByPlaceholderText(textValue + '*')).toBeFalsy();
+    expect(screen.queryByPlaceholderText(textValue)).toBeTruthy();
+  });
+});
--- client/src/components/toggleRequiredInput/toggleRequiredInput.test.tsx
+++ client/src/components/toggleRequiredInput/toggleRequiredInput.test.tsx
@@ -2,30 +2,30 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ToggleButton from './toggleRequiredInput';
 
-const mockSetRequired = jest.fn();
+const setRequiredMock = jest.fn();
 
 describe('<ToggleButton />', () => {
   const requiredValue = true;
 
   test('renders ToggleButton', () => {
     render(
-      <ToggleButton required={requiredValue} setRequired={mockSetRequired} />
+      <ToggleButton required={requiredValue} setRequired={setRequiredMock} />
     );
     expect(screen.queryByTestId('toggle-button-' + requiredValue)).toBeTruthy();
   });
 
   test('calls setRequiredFunction when toggled', async () => {
     render(
-      <ToggleButton required={requiredValue} setRequired={mockSetRequired} />
+      <ToggleButton required={requiredValue} setRequired={setRequiredMock} />
     );
     expect(screen.queryByTestId('toggle-button-' + requiredValue)).toBeTruthy();
     await userEvent.click(screen.getByTestId('toggle-button-' + requiredValue));
-    expect(mockSetRequired).toHaveBeenCalled();
+    expect(setRequiredMock).toHaveBeenCalled();
   });
 
   test('shows On state when required', async () => {
     render(
-      <ToggleButton required={requiredValue} setRequired={mockSetRequired} />
+      <ToggleButton required={requiredValue} setRequired={setRequiredMock} />
     );
     expect(screen.queryByText('On')).toBeTruthy();
     expect(screen.queryByText('Off')).toBeFalsy();
@@ -33,7 +33,7 @@ describe('<ToggleButton />', () => {
 
   test('shows Off state when not required', async () => {
     render(
-      <ToggleButton required={!requiredValue} setRequired={mockSetRequired} />
+      <ToggleButton required={!requiredValue} setRequired={setRequiredMock} />
     );
     expect(screen.queryByText('On')).toBeFalsy();
     expect(screen.queryByText('Off')).toBeTruthy();
