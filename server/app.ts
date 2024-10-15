2457a5a2ecd2803f16227219c0cb86b7e4cb21ee
Brayan Durazo <brayan9612@gmail.com>
2024-10-14 21:19:08 -0700

    Implement sq lite database (#5)
    * install sqlite3 package
    
    * add missing files to installed sqlite3 package
    
    * add SQLite database to the server
    
    * remove development provisional data

--- server/app.ts
+++ server/app.ts
@@ -2,7 +2,43 @@ import express, { Request, Response } from "express";
 import formWidgetRouter from './routes/formWidget.router'
 import cors from 'cors';
 
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
+
+export const db = new sqlite3.Database('./database/formWidgetDB.db', (err: { message: any; }) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the formWidgetDB database.');
+});
+
+const createTableSql = `
+    CREATE TABLE IF NOT EXISTS FormWidget (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        title_textContent TEXT NOT NULL,
+        title_fontSize INTEGER NOT NULL,
+        title_fontColor TEXT NOT NULL,
+        title_alignment TEXT CHECK (title_alignment IN ('left', 'center', 'right')) NOT NULL,
+        
+        subTitle_textContent TEXT NOT NULL,
+        subTitle_fontSize INTEGER NOT NULL,
+        subTitle_fontColor TEXT NOT NULL,
+        subTitle_alignment TEXT CHECK (subTitle_alignment IN ('left', 'center', 'right')) NOT NULL,
+        
+        submitButton_textContent TEXT NOT NULL,
+        submitButton_backgroundColor TEXT NOT NULL,
+        
+        requiredFirstName BOOLEAN NOT NULL,
+        requiredLastName BOOLEAN NOT NULL
+    );`
+
+db.run(createTableSql, (err: { message: string; }) => {
+    if (err) {
+        return console.error('Error creating table:', err.message);
+    }
+    console.log('Table created successfully');
+});
 
 app.use(express.json());
 app.use(cors());
--- server/routes/formWidget.router.ts
+++ server/routes/formWidget.router.ts
@@ -1,20 +1,21 @@
 import {Router, Request, Response } from "express";
-import formWidgets from "../data/formWidgetData";
-import { getNewFormWidgetId } from "../controller/formWidgetController";
+import { flattenFormWidget } from "../controller/formWidgetController";
+import {db} from "../app"
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-    try{
-        const data = await new Promise(resolve => setTimeout(() => resolve(formWidgets), 500));
-        res.status(200).send(data);
-    }catch(error){
-        res.status(500).send({ message: 'Something went wrong' });
-    }
+    db.all('SELECT * FROM FormWidget', (err: { message: any; }, rows: any) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Internal server error');
+        } else {
+            res.send(rows);
+        }
+    });
 });
 
 router.post("/", (req: Request, res: Response) => {
     const newFormWidget = {
-        id: getNewFormWidgetId(),
         title: {
             textContent: req.body.title.textContent,
             fontSize: req.body.title.fontSize,
@@ -34,43 +35,41 @@ router.post("/", (req: Request, res: Response) => {
         requiredFirstName: req.body.requiredFirstName,
         requiredLastName: req.body.requiredLastName,
     }
-    formWidgets.push(newFormWidget)
+    const flatFormWidget = flattenFormWidget(newFormWidget)
+    const sql = `
+    INSERT INTO FormWidget (
+        title_textContent, title_fontSize, title_fontColor, title_alignment,
+        subTitle_textContent, subTitle_fontSize, subTitle_fontColor, subTitle_alignment,
+        submitButton_textContent, submitButton_backgroundColor,
+        requiredFirstName, requiredLastName
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+  
+    const values = [
+        flatFormWidget.title_textContent,
+        flatFormWidget.title_fontSize,
+        flatFormWidget.title_fontColor,
+        flatFormWidget.title_alignment,
+        flatFormWidget.subTitle_textContent,
+        flatFormWidget.subTitle_fontSize,
+        flatFormWidget.subTitle_fontColor,
+        flatFormWidget.subTitle_alignment,
+        flatFormWidget.submitButton_textContent,
+        flatFormWidget.submitButton_backgroundColor,
+        flatFormWidget.requiredFirstName,
+        flatFormWidget.requiredLastName
+    ];
+
+    db.run(sql, values, function(err: { message: any; }) {
+        if (err) {
+            return console.error(err.message);
+        }
+        // @ts-ignore comment
+        console.log(`Row inserted with rowId ${this.lastID}`);
+    });
+
     res.status(201).json(newFormWidget);
     return;
-});
-
-router.put('/:id', (req: Request, res: Response) => {
-    const id = parseInt(req.params.id);
-    const formWidget = formWidgets.find((formWidget) => formWidget.id === id);
-    if (!formWidget) {
-        res.status(404).send({ error: "Form widget not found" });
-        return;
-    }
-    formWidget.title.textContent = req.body.title.textContent || formWidget.title.textContent
-    formWidget.title.fontSize = req.body.title.fontSize || formWidget.title.fontSize
-    formWidget.title.fontColor = req.body.title.fontColor || formWidget.title.fontColor
-    formWidget.title.alignment = req.body.title.alignment || formWidget.title.alignment
-    formWidget.subTitle.textContent = req.body.subTitle.textContent || formWidget.subTitle.textContent
-    formWidget.subTitle.fontSize = req.body.subTitle.fontSize || formWidget.subTitle.fontSize
-    formWidget.subTitle.fontColor = req.body.subTitle.fontColor || formWidget.subTitle.fontColor
-    formWidget.subTitle.alignment = req.body.subTitle.alignment || formWidget.subTitle.alignment
-    formWidget.submitButton.textContent = req.body.submitButton.textContent || formWidget.submitButton.textContent
-    formWidget.submitButton.backgroundColor = req.body.submitButton.backgroundColor || formWidget.submitButton.backgroundColor
-    formWidget.requiredFirstName = req.body.requiredFirstName || formWidget.requiredFirstName
-    formWidget.requiredLastName = req.body.requiredLastName || formWidget.requiredLastName
-
-    res.json(formWidget);
-});
-  
-router.delete("/:id", (req: Request, res: Response) => {
-    const id = parseInt(req.params.id);
-    const index = formWidgets.findIndex((formWidget) => formWidget.id === id);
-    if (index === -1) {
-        res.status(404).json({ error: "Form widget not found" });
-        return;
-    }
-    formWidgets.splice(index, 1);
-    res.status(204).send();
 });
 
 export default router;
