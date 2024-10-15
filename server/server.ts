e3dde589c18d1cf35de4069cac443790a45a7f4f
Brayan Durazo <brayan9612@gmail.com>
2024-10-14 23:53:01 -0700

    add port directly to ts file

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,8 @@
 import app from './app'
 
 require("dotenv").config();
-const port = process.env.PORT
+const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server listening on port ${port}`)
 })
