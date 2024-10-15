c80a679da6ef17e3a66e111c4a89d714c460dfeb
Brayan Durazo <brayan9612@gmail.com>
2024-10-15 13:38:22 -0700

    add test to font size input

---
+++ client/src/components/fontSizeInput/fontSizeInput.test.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import FontSizeInput from './fontSizeInput';
+
+const setFontSizeMock = jest.fn();
+
+describe('<FontSizeInput />', () => {
+  const fontSize = 8;
+
+  afterEach(() => {
+    setFontSizeMock.mockClear();
+  });
+
+  test('renders FontSizeInput', () => {
+    render(<FontSizeInput fontSize={fontSize} setFontSize={setFontSizeMock} />);
+    expect(screen.queryByTestId('font-size-input')).toBeTruthy();
+  });
+
+  test('calls setFontSizeMock when number input clicked', async () => {
+    render(<FontSizeInput fontSize={fontSize} setFontSize={setFontSizeMock} />);
+
+    expect(screen.queryByTestId('font-size-input')).toBeTruthy();
+
+    fireEvent.change(screen.getByTestId('font-size-input'), {
+      target: { value: '24' },
+    });
+
+    expect(setFontSizeMock).toHaveBeenCalled();
+  });
+});
